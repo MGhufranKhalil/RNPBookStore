c64bb50d54c42472772ef615dde524ca3b531557
Babar <babar.bahadur@hotmail.com>
2019-04-24 19:52:15 +0500

    Commit by Babar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View, Button, Image, StyleSheet} from 'react-native';
+import { Text, View, Button, Image, StyleSheet, ScrollView } from 'react-native';
 
 //Navigation Imports
 import { createStackNavigator, createAppContainer,  } from 'react-navigation';
 
+// import { TextInput } from "react-native";
+import { CustomButton, Card, CardSection, Input } from "./components/common";
 
-//Introction Screen of the Library App
+
+//Introduction Screen of the Library App
 class Introduction   extends Component {
   static navigationOptions = {
     title: 'Welcome Screen'   //it shows 'Welcome Screen' on the top
@@ -14,22 +17,30 @@ class Introduction   extends Component {
 
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      
+      <ScrollView> 
       <Image
           style={{width: 200, height: 300}}
           source={{uri: 'https://requestreduce.org/images/clip-art-for-free-6.png'}}
         />
-
-        <Text style={styles.welcome}>Welcome to Book Store</Text>
       
-      
+        
         <Button
-          title="Click to Enter"
-          onPress={() => this.props.navigation.navigate('Education', {
+          title="Enter as Admin"
+          onPress={() => this.props.navigation.navigate('AdminLoginScreen', {
             itemId: 12,
-            otherMsg: "Sent to Education"
+            otherMsg: "Sent to Login Form"
           })}
         />
+      
+      <Button
+          title="Enter as User"
+          onPress={() => this.props.navigation.navigate('UserLoginScreen', {
+            itemId: 12,
+            otherMsg: "Sent to Login Form"
+          })}
+        />
+
+      </ScrollView> 
       </View>
 
       
@@ -37,6 +48,102 @@ class Introduction   extends Component {
     );
   }
 }
+
+
+//Login form for Admin
+class AdminLoginForm extends Component {
+
+  static navigationOptions = {
+    title: 'Admin Login Screen'   //it shows 'Welcome Screen' on the top
+  }
+
+  // To handle the text input we need the state in the action
+  state = { email: "", password: "" };
+  render() {
+  return (
+  <Card>
+  {/* For User ID */}
+  <CardSection>
+  {/* When the text input is not treated as a custom component 
+  <TextInput
+  value={this.state.text}
+  onChangeText={text => this.setState({ text: text })}
+  style={{ height: 20, width: 100 }}
+  /> */}
+  <Input
+  autoCorrect
+  placeholder="admin@email.com"
+  label="Email: "
+  value={this.state.email}
+  onChangeText={email => this.setState({ email })}
+  />
+  </CardSection>
+  {/* For Password */}
+  <CardSection>
+  <Input
+  secureTextEntry
+  placeholder="password"
+  label="Password"
+  value={this.state.password}
+  onChangeText={password => this.setState({ password })}
+  />
+  </CardSection>
+  {/* For the Login Button */}
+  <CardSection>
+  <CustomButton>Sign In</CustomButton>
+  </CardSection>
+  </Card>
+  );
+  }
+  }
+
+
+//Login form for User
+class UserLoginForm extends Component {
+
+  static navigationOptions = {
+    title: 'User Login Screen'   //it shows 'Welcome Screen' on the top
+  }
+
+  // To handle the text input we need the state in the action
+  state = { email: "", password: "" };
+  render() {
+  return (
+  <Card>
+  {/* For User ID */}
+  <CardSection>
+  {/* When the text input is not treated as a custom component 
+  <TextInput
+  value={this.state.text}
+  onChangeText={text => this.setState({ text: text })}
+  style={{ height: 20, width: 100 }}
+  /> */}
+  <Input
+  autoCorrect
+  placeholder="user@email.com"
+  label="Email: "
+  value={this.state.email}
+  onChangeText={email => this.setState({ email })}
+  />
+  </CardSection>
+  {/* For Password */}
+  <CardSection>
+  <Input
+  secureTextEntry
+  placeholder="password"
+  label="Password"
+  value={this.state.password}
+  onChangeText={password => this.setState({ password })}
+  />
+  </CardSection>
+  {/* For the Login Button */}
+  <CardSection>
+  <CustomButton>Sign In</CustomButton>
+  </CardSection>
+  </Card>
+  );
+  }
+  }
 
 
 //Education Screen View Settings
@@ -79,46 +186,12 @@ class EducationScreen extends Component {
 
 
 
-
-
-//Experience Screen View Settings
-class ExperienceScreen extends Component {
-  static navigationOptions = {
-    title: 'Experience Screen'
-  }
-  render() {
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherMsg = navigation.getParam('otherMsg', 'This is the default Msg');
-
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Hard coding</Text>
-        
-        <Button
-          title="Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-
-      </View>
-    );
-  }
-}
-
-
-
-
-
 //Creation of the Stack Navigator
 const AppNavigator = createStackNavigator({
   Home: Introduction,
   Education: EducationScreen,
-  Experience: ExperienceScreen
+  AdminLoginScreen: AdminLoginForm,
+  UserLoginScreen: UserLoginForm
 },
   {
     initialRouteName: "Home"
---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+export * from "./CustomButton";
+export * from "./Card";
+export * from "./CardSection";
+export * from "./Header";
+export * from "./Input";
