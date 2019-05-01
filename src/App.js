55684c602bc6774e266676f644d8ef36bcebcb13
Babar <babar.bahadur@hotmail.com>
2019-05-01 21:16:36 +0500

    Babar's commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { Text, View, Button, Image, StyleSheet, ScrollView } from 'react-native'
 //Navigation Imports
 import { createStackNavigator, createAppContainer,  } from 'react-navigation';
 
-// import { TextInput } from "react-native";
-import { CustomButton, Card, CardSection, Input } from "./components/common";
+import firebase from "firebase";
 
+// import { TextInput } from "react-native";
+import { CustomButton, Card, CardSection, Input, Spinner } from "./components/common";
 
 //Introduction Screen of the Library App
 class Introduction   extends Component {
@@ -52,61 +53,61 @@ class Introduction   extends Component {
 
 //Login form for Admin
 class AdminLoginForm extends Component {
-
-  static navigationOptions = {
-    title: 'Admin Login Screen'   //it shows 'Welcome Screen' on the top
-  }
-
   // To handle the text input we need the state in the action
-  state = { email: "", password: "" };
-  render() {
+  state = { email: "", password: "", error: "", loading: false };
+  
+  //Helper method onButtonPress() to Login the user
+  onButtonPress() {
+  const { email, password } = this.state;
+  //Firebase mathod to login using userid & password
+  
+  //Clear out the Error Message on Every Login Attempt
+  this.setState({ error: "", loading: true });
+  
+
+  firebase
+  .auth()     //Get the auth service for the default app
+  .signInWithEmailAndPassword(email, password)
+  .then(this.onLoginSuccess.bind(this))
+  .catch(() => {
+  firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then(this.onLoginSuccess.bind(this))
+  .catch(this.onLoginFailed.bind(this));
+  // .catch(() => {
+  // this.setState({ error: "Authentication Failed" });
+  // });
+  //Creates a new user account associated with the specified email address and password. On successful creation of the user account, this user will also be signed in to your application.
+  });
+  }
+  
+  onLoginFailed() {
+  this.setState({
+  error: "Authentication Failed",
+  loading: false
+  });
+  }
+  onLoginSuccess() {
+  this.setState({
+  email: "",
+  password: "",
+  loading: false,
+  error: ""
+  });
+  }
+  
+  renderButton() {
+  if (this.state.loading) {
+  return <Spinner size="small" />;
+  }
   return (
-  <Card>
-  {/* For User ID */}
-  <CardSection>
-  {/* When the text input is not treated as a custom component 
-  <TextInput
-  value={this.state.text}
-  onChangeText={text => this.setState({ text: text })}
-  style={{ height: 20, width: 100 }}
-  /> */}
-  <Input
-  autoCorrect
-  placeholder="admin@email.com"
-  label="Email: "
-  value={this.state.email}
-  onChangeText={email => this.setState({ email })}
-  />
-  </CardSection>
-  {/* For Password */}
-  <CardSection>
-  <Input
-  secureTextEntry
-  placeholder="password"
-  label="Password"
-  value={this.state.password}
-  onChangeText={password => this.setState({ password })}
-  />
-  </CardSection>
-  {/* For the Login Button */}
-  <CardSection>
-  <CustomButton>Sign In</CustomButton>
-  </CardSection>
-  </Card>
+  <CustomButton onPress={this.onButtonPress.bind(this)}>
+  Sign In
+  </CustomButton>
   );
   }
-  }
-
-
-//Login form for User
-class UserLoginForm extends Component {
-
-  static navigationOptions = {
-    title: 'User Login Screen'   //it shows 'Welcome Screen' on the top
-  }
-
-  // To handle the text input we need the state in the action
-  state = { email: "", password: "" };
+  
   render() {
   return (
   <Card>
@@ -136,9 +137,115 @@ class UserLoginForm extends Component {
   onChangeText={password => this.setState({ password })}
   />
   </CardSection>
+  {/* For the Error Message */}
+  <Text style={styles.errorTextStyle}>{this.state.error}</Text>
   {/* For the Login Button */}
   <CardSection>
-  <CustomButton>Sign In</CustomButton>
+  {this.renderButton()}
+  {/* Moved to renderButton() <CustomButton onPress={this.onButtonPress.bind(this)}>
+  Sign In
+  </CustomButton> */}
+  </CardSection>
+  </Card>
+  );
+  }
+  }
+
+
+//Login form for User
+class UserLoginForm extends Component {
+  // To handle the text input we need the state in the action
+  state = { email: "", password: "", error: "", loading: false };
+  
+  //Helper method onButtonPress() to Login the user
+  onButtonPress() {
+  const { email, password } = this.state;
+  //Firebase mathod to login using userid & password
+  
+  //Clear out the Error Message on Every Login Attempt
+  this.setState({ error: "", loading: true });
+  
+
+  firebase
+  .auth()     //Get the auth service for the default app
+  .signInWithEmailAndPassword(email, password)
+  .then(this.onLoginSuccess.bind(this))
+  .catch(() => {
+  firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then(this.onLoginSuccess.bind(this))
+  .catch(this.onLoginFailed.bind(this));
+  // .catch(() => {
+  // this.setState({ error: "Authentication Failed" });
+  // });
+  //Creates a new user account associated with the specified email address and password. On successful creation of the user account, this user will also be signed in to your application.
+  });
+  }
+  
+  onLoginFailed() {
+  this.setState({
+  error: "Authentication Failed",
+  loading: false
+  });
+  }
+  onLoginSuccess() {
+  this.setState({
+  email: "",
+  password: "",
+  loading: false,
+  error: ""
+  });
+  }
+  
+  renderButton() {
+  if (this.state.loading) {
+  return <Spinner size="small" />;
+  }
+  return (
+  <CustomButton onPress={this.onButtonPress.bind(this)}>
+  Sign In
+  </CustomButton>
+  );
+  }
+  
+  render() {
+  return (
+  <Card>
+  {/* For User ID */}
+  <CardSection>
+  {/* When the text input is not treated as a custom component 
+  <TextInput
+  value={this.state.text}
+  onChangeText={text => this.setState({ text: text })}
+  style={{ height: 20, width: 100 }}
+  /> */}
+  <Input
+  autoCorrect
+  placeholder="user@email.com"
+  label="Email: "
+  value={this.state.email}
+  onChangeText={email => this.setState({ email })}
+  />
+  </CardSection>
+  {/* For Password */}
+  <CardSection>
+  <Input
+  secureTextEntry
+  placeholder="password"
+  label="Password"
+  value={this.state.password}
+  onChangeText={password => this.setState({ password })}
+  />
+  </CardSection>
+  {/* For the Error Message */}
+  <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+  {/* For the Login Button */}
+  <CardSection>
+  {this.renderButton()}
+  {/* Moved to renderButton() <CustomButton onPress={this.onButtonPress.bind(this)}>
+  Sign In
+  </CustomButton> */}
   </CardSection>
   </Card>
   );
